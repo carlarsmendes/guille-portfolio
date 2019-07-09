df888fd981bb5f69062612631fbff67302706428
Carla Mendes <carlarsmendes@gmail.com>
2019-07-09 12:34:17 +0100

    changes on js working words

--- javascript/script.js
+++ javascript/script.js
@@ -1,29 +1,24 @@
 // Here to write the random words(so that they're not in CSS anymore)
-const $randomWord = document.getElementsByClassName("randomWord");
-
-
-
+const $randomWord = document.getElementById("randomWord");
 
 setInterval(function randomWords() {
   // Random number generator
   let wordsArr = ["Service Designer", "Sushi Lover", "Design Thinker"];
   let randomIndex = Math.round(Math.random() * (wordsArr.length - 1));
   // jQuery: Change the word in the span for a random one in the array of words
+  console.log("wordsArr[randomIndex]", wordsArr[randomIndex]);
   $randomWord.innerText = wordsArr[randomIndex];
-  //   console.log(wordsArr[randomIndex]);
-  //   console.log($randomWord);
+  //console.log("$randomWord", $randomWord);
+  console.log("$randomWord.innerText", $randomWord.innerText);
 }, 2000); //2000 for every 2 seconds, change later if necessary
 
-//console element that is overflowing the screen width --- 
+//console element that is overflowing the screen width ---
 //very useful https://dev.to/drews256/ridiculously-easy-row-and-column-layouts-with-flexbox-1k01
 
 var docWidth = document.documentElement.offsetWidth;
 
-[].forEach.call(
-  document.querySelectorAll('*'),
-  function(el) {
-    if (el.offsetWidth > docWidth) {
-      console.log(el);
-    }
+[].forEach.call(document.querySelectorAll("*"), function(el) {
+  if (el.offsetWidth > docWidth) {
+    console.log(el);
   }
-);
+});
