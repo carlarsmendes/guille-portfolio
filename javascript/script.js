1e9b12a324be6d8b0e6a7b5a54f6f922248dd961
Carla Mendes <carlarsmendes@gmail.com>
2019-07-12 11:25:01 +0100

    changes nav bar media query <320

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 // Here to write the random words(so that they're not in CSS anymore)
-const $randomWord = document.getElementById("randomWord");
+const $randomWord = document.getElementById("js-randomWord");
 
 const $mainSection = document.getElementById("main-section");
 
