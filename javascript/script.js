dc0ff29d94e60bb596a14b189c07e6db02ac35e3
Carla Mendes <carlarsmendes@gmail.com>
2019-07-16 10:46:29 +0100

    changes nav bar

--- javascript/script.js
+++ javascript/script.js
@@ -78,3 +78,14 @@ var docWidth = document.documentElement.offsetWidth;
     console.log(el);
   }
 });
+
+// ----------Responsive Burgur Menu JS---------
+
+// function myFunction() {
+//   var x = document.getElementById("navbar");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
